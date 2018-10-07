21ae971cf7fb4f29fd3de0f10cdd7bd51a723b77
argaitt <argaitt@gmail.com>
2018-10-07 15:51:02 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let n = primeDivisors(base), tmp = n, result = 0;
+	while(number > n){
+		result = result + Math.floor(number / n);
+		n = n * tmp;
+	}
+	return result;
+}
+
+function primeDivisors(base){
+	let result = [], number = base, curNumber = number, probe = 2;
+	while(curNumber != 1){
+		if(curNumber % probe != 0){
+			probe++;
+		}
+		else{
+			curNumber = curNumber / probe;
+			result.push(probe);
+		}
+	}
+	let max = result[0];
+	for(let i = 0; i < result.length; i++){
+		if(max < result[i]){
+			max = result[i];
+		}
+	}
+	return max;
 }
